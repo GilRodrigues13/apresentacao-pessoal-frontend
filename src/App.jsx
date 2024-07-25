8f84a25632a4fd88db22ad4a2a00b3ed5e190b46
unknown <gilgilberto.s.rodrigues@gmail.com>
2024-07-25 14:38:11 -0300

    FEAT: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
 
-import './App.css'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
-      </main>
-    </>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300  selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full"></div>
+     <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="container mx-auto px-8"></div>
+      <Header />
+      
+    </div>
   );
 }
 
-export default App
+export default App;
