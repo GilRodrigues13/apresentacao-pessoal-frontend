6fc1e9b0649922247d0ad6745561ac08c480dbe6
Gil Rodrigues <gilgilberto.s.rodrigues@gmail.com>
2024-07-25 20:35:06 -0300

    CHORE: update dependencies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,14 @@ import {FaInstagram} from "react-icons/fa";
 
 const Header = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
+    <nav className="mb-19 flex items-center justify-between py-6 -mt-14">
     <div className="flex flex-shrink-0 items-center"> 
-        <img className="mx-2 w-10"  src={logo} alt="Logo"/>
+        <img className="mx-2"  src={logo} alt="Logo"/>
     </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub/>
-        <FaInstagram />
+    <div className="m-9 flex items-center justify-center gap-4 text-3xl">
+        <a href="https://www.linkedin.com/in/gil-rodrigues-27bb60268/" target="_blank" ><FaLinkedin /></a>
+        <a href="https://github.com/GilRodrigues13" target="_blank" ><FaGithub/></a>
+        <a href="https://www.instagram.com/gilgilbertosr/" target="_blank" ><FaInstagram /></a>
     </div>
   </nav>
   );
