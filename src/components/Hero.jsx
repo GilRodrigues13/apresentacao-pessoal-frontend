3650bd7bc2c7defa81bc520e7b9568b3d3f62158
Gil Rodrigues <gilgilberto.s.rodrigues@gmail.com>
2024-07-26 02:02:11 -0300

    REFACTOR: clean up code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,15 @@ import profilePic from "../assets/GilProfile.png"
 const Hero = () => {
   return (
 
-     <div className="border-b border-purple-800 pb-4 lg:mb-35">
+     <div >
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
             
                 <div className="flex flex-col items-center lg:items-start">
                     <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-40 lg:text-8xl mx-8
                     ">Gil Rodrigues</h1>
-                    <span className="bg-gradient-to-r from-orange-300 via-orange-500 to-red-600 bg-clip-text text-4xl tracking-tight text-transparent mx-10 -mt-10">Front-End Developer</span>
+                    <span className="bg-gradient-to-r from-orange-300 via-orange-500 to-red-600 bg-clip-text text-3xl tracking-tight text-transparent mx-14 -mt-10 
+                    ">Front-End Developer</span>
                     
                 </div>
             </div>
