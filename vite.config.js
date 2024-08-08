5c8f1c58c853b68e346383bbf855ed74f2f59518
Gil Rodrigues <gilgilberto.s.rodrigues@gmail.com>
2024-08-07 21:43:52 -0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/GilRodrigues13/apresentacao-pessoal-frontend.git'
+  base: '/apresentacao-pessoal-frontend/'
 })
